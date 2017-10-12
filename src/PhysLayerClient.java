50121244a2b56f7525d70e285e09fa3f8e172a2e
Adrian Cuellar <aacuellar14@gmail.com>
2017-10-11 21:55:53 -0700

    more progress

--- src/PhysLayerClient.java
+++ src/PhysLayerClient.java
@@ -1,37 +1,63 @@
-import java.io.*;
-import java.net.Socket;
-public final class PhysLayerClient {
-    public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("18.221.102.182",38002)) {
-            System.out.println("Connected to server.");
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            BufferedReader brIS = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            double baseline =0;
-            for(int i=0;i<64;i++){
-                baseline+=br.read();
-            }
-            baseline/=64;
-           int[] b = new int[320];
-            System.out.println("Established baseline is "+baseline);
-          for(int i=0;i<320;i++){
-               if(baseline<br.read()) {
-                   b[i] = 1;
+    import java.io.*;
+    import java.net.Socket;
+    public final class PhysLayerClient {
+        public static void main(String[] args) throws Exception {
+            try (Socket socket = new Socket("18.221.102.182",38002)) {
+                System.out.println("Connected to server.");
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                BufferedReader brIS = new BufferedReader(new InputStreamReader(System.in));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                double baseline =0;
+                for(int i=0;i<64;i++){
+                    baseline+=br.read();
+                }
+                baseline/=64;
+               int[] b = new int[320];
+                System.out.println("Established baseline is "+baseline);
+              for(int i=0;i<320;i++){
+                   if(baseline<br.read()) {
+                       b[i] = 1;
+                   }
+                   else {
+                       b[i] = 0;
+                   }
                }
-               else {
-                   b[i] = 0;
-               }
+               String[] fiveBytes = new String[64];
+                //Decode from NRZI
+                //CURRENTLY CONTINUOUS
+                for(int i=0;i<64;i++){
+                    String byteString="";
+                    for(int j=0;j<5;j++){
+                        if(((i*5)+j)!=0){
+                            if(b[(i*5)+j]!=b[(i*5)+j-1])
+                                byteString+=1;
+                            else
+                                byteString+=0;
+                        }
+                        else{
+                            if(b[0]!=0)
+                                byteString+=1;
+                            else
+                                byteString+=0;
+                        }
+                    }
+                    fiveBytes[i]=byteString;
+                }
+            //DECODE from 5B to 4B
+           String binaryHolder="";
+           for(int i=0;i<64;i+=1){
+               binaryHolder+=fiveBtoFourB(fiveBytes[i]);
            }
-           // NEED TO DECODE FROM NRZI FIRST
-           String[] binaryHolder = new String[64];
-            int place=0;
-           for(int i=0;i<320;i+=5){
-               binaryHolder[place]=fiveBtoFourB(""+b[i]+b[i+1]+b[i+2]+b[i+3]+b[i+4]);
-                place++;
+           String hexStr="";
+                //index out of bounds when binaryHolder is short
+           for(int i=0;i<64;i++){
+               int deci = Integer.parseInt(binaryHolder.substring(0+(4*i),4+(4*i)),2);
+               hexStr+=Integer.toHexString(deci);
            }
-
+           out.println(hexStr.toUpperCase());
+           System.out.println(hexStr.toUpperCase());
            if(br.read()==1)
                 System.out.println("Response good");
             else
